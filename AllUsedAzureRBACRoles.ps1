821486899d2de4898f94739882a03ff5833ff8a4
Alcapsulator <54436611+Alcapsulator@users.noreply.github.com>
2019-08-23 11:26:40 +0200

    Add files via upload

---
+++ AllUsedAzureRBACRoles.ps1
@@ -0,0 +1,28 @@
+﻿try {
+
+    $roles = Get-AzureADDirectoryRole
+
+    function GetRBACRoles($roles) {
+        $rbac_roles = @()
+        Foreach ($r in $roles) {
+            GetRBACRoleMembers $r.ObjectId
+        }
+        return $rbac_roles
+    }
+
+    function GetRBACRoleMembers($currentrole) {
+        $rbac_members = @()
+        Foreach($u in Get-AzureADDirectoryRoleMember -ObjectId $currentrole) {
+            $rbac_members += [PSCustomObject]@{Role_OId = $r.ObjectId; Role=$r.DisplayName; Description=$r.Description; Member = $u.Displayname }
+        }
+        return $rbac_members
+    }
+
+    Echo "Getting all roles"
+    $myrole = GetRBACRoles $roles
+
+} catch {
+    echo "Erorr $($_.Exception.Message)"
+    pause
+    break
+}
